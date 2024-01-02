82f6200706d98374715333a72e19b34f70501466
陳柏勳 <3B032013@gm.student.ncut.edu.tw>
2024-01-02 15:08:58 +0800

    update

--- app.py
+++ app.py
@@ -65,6 +65,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
+    msg = event.message.text
+    channel_id = '2384494'
+    api_key = 'EJU2GGIUNTGCOV4S'
     if '最新合作廠商' in msg:
         message = imagemap_message()
         line_bot_api.reply_message(event.reply_token, message)
