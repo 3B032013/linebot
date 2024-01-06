9754de7de1e0ff8c334e53429aa24ee9f36f410c
陳柏勳 <3B032013@gm.student.ncut.edu.tw>
2024-01-06 19:46:07 +0800

    update

--- app.py
+++ app.py
@@ -75,25 +75,25 @@ def handle_message(event):
     msg = event.message.text
     channel_id = '2384494'
     api_key = 'EJU2GGIUNTGCOV4S'
-    if '最新合作廠商' in msg:
-        message = imagemap_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '最新活動訊息' in msg:
-        message = buttons_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '註冊會員' in msg:
-        message = Confirm_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '旋轉木馬' in msg:
-        message = Carousel_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '圖片畫廊' in msg:
-        message = test()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '功能列表' in msg:
-        message = function_list()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '溫度' in msg:
+#     if '最新合作廠商' in msg:
+#         message = imagemap_message()
+#         line_bot_api.reply_message(event.reply_token, message)
+#     elif '最新活動訊息' in msg:
+#         message = buttons_message()
+#         line_bot_api.reply_message(event.reply_token, message)
+#     elif '註冊會員' in msg:
+#         message = Confirm_Template()
+#         line_bot_api.reply_message(event.reply_token, message)
+#     elif '旋轉木馬' in msg:
+#         message = Carousel_Template()
+#         line_bot_api.reply_message(event.reply_token, message)
+#     elif '圖片畫廊' in msg:
+#         message = test()
+#         line_bot_api.reply_message(event.reply_token, message)
+#     elif '功能列表' in msg:
+#         message = function_list()
+#         line_bot_api.reply_message(event.reply_token, message)
+    if '溫度' in msg:
         # Fetch temperature from ThingSpeak
         temperatures = fetch_thingspeak_data(channel_id, api_key, 'field1')
         if temperatures:
